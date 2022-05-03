b52a4b80880b08bac8fde740eca697559b5082c6
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-05-03 12:35:28 +0800

    Update and rename change_state_event_example.js to change_state_event_custom.js

---
+++ change_state_event_custom.js
@@ -0,0 +1,62 @@
+/**
+ * change_state_event_example.js - taffy-layout-parser
+ */
+
+/**
+ * [Proxy] - [dispatchEvent]
+ *    |    /
+ *  [Element]
+ */
+
+const changeEvent = new Event('change');
+const page = document.createElement('div');
+
+page.dispatchEvent(changeEvent);
+
+document.body.appendChild(page);
+
+const event_buck = {
+  listen: 'count',
+  buck: [],
+};
+
+function Buck(el) {
+  event_buck.buck.push(el);
+}
+
+const state = new Proxy(
+  { count: 0 },
+  {
+    get(t, k, r) {},
+    set(t, k, v) {
+      const _value = v;
+
+      if (typeof t[k] !== 'undefined') {
+        if (!Object.is(t[k], _value)) {
+          t[k] = _value;
+          if (k === event_buck.listen) {
+            event_buck.buck.forEach((el) => {
+              el.dispatchEvent(changeEvent);
+            });
+          }
+        }
+      }
+    },
+  }
+);
+
+page.addEventListener('change', () => {
+  console.log('DONE');
+});
+
+Buck(page);
+
+const dispatch_0 = document.querySelector('#dispatch_0');
+const dispatch_1 = document.querySelector('#dispatch_1');
+
+dispatch_0.onclick = function () {
+  state.count = 22;
+};
+dispatch_1.onclick = function () {
+  state.count = 0;
+};
--- change_state_event_example.js
+++
@@ -1,9 +0,0 @@
-/**
- * change_state_event_example.js - taffy-layout-parser
- */
-
-/**
- * [Proxy] - [dispatchEvent]
- *    |    /
- *  [Element]
- */
