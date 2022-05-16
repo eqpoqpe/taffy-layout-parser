d3513583ad239a13c164af5681119c99145bc88b
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-05-16 13:05:11 +0800

    Update index.js

--- src/present/index.js
+++ src/present/index.js
@@ -10,11 +10,58 @@
 
 export default function naivePresent() {
   return {
-    
-    /**
-     *
-     * @params {string|symbol} s
-     */
-    onEvent(s) {}
+    onEvent: {
+
+      /**
+       *
+       * @param {string|symbol} s
+       * @returns {{effect: Function, on: Function}}
+       */
+      create(s) {
+        const _that = this[s] = {
+
+          // user defined
+          _$: 1,
+          handle: null,
+
+          // event type
+          type: null,
+          once: false,
+
+          // after at component mounted
+          // default is before
+          after: false,
+        };
+
+        return {
+
+          /**
+           * 
+           * @param {Function} fn 
+           * @param {*} fn_args 
+           */
+          effect(fn, ...fn_args) {
+            _that.handle = typeof fn === "function" ? fn : null;
+
+            // @event
+            // prue data
+            if (fn_args.length > 0) { }
+          },
+
+          /**
+           * 
+           * @param {string} et event type 
+           */
+          on(et) {
+            _that.type = et;
+
+            return {
+              once() { _that.once = true; },
+              after() { _that.after = true; }
+            }
+          },
+        }
+      }
+    }
   };
 };
